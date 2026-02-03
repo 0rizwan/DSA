b3982ef69beb0c0e5bde3d3d0c3447ba97aadb0c
0rizwan <fycsrizwan@gmail.com>
2026-02-03 13:06:56 +0530

    pattern 8 and 9

--- dsa_with_java/src/basics/patterns.java
+++ dsa_with_java/src/basics/patterns.java
@@ -87,6 +87,73 @@ public class patterns {
         }
     }
 
+    // ----*----
+    // ---***---
+    // --*****--
+    // -*******-
+    // *********
+    static void pattern7(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            for (int l = 0; l < n - i - 1; l++) {
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    // *********
+    // -*******-
+    // --*****--
+    // ---***---
+    // ----*----
+    static void pattern8(int n) {
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            for (int l = 0; l < n - i; l++) {
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+
+    static void pattern9(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            for (int l = 0; l < n - i - 1; l++) {
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            for (int l = 0; l < n - i; l++) {
+                System.out.print(" ");
+            }
+            System.out.println("");
+        }
+    }
+
     public static void main(String[] args) {
         pattern1(4);
         pattern2(5);
@@ -94,5 +161,8 @@ public class patterns {
         pattern4(5);
         pattern5(5);
         pattern6(5);
+        pattern7(7);
+        pattern8(7);
+        pattern9(10);
     }
 }
