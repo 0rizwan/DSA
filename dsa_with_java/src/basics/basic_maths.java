acd4c84126b7ea2c28a9be919f15eb068bc5805f
0rizwan <fycsrizwan@gmail.com>
2026-02-22 02:50:47 +0530

    basic maths done

--- dsa_with_java/src/basics/basic_maths.java
+++ dsa_with_java/src/basics/basic_maths.java
@@ -92,8 +92,8 @@ public class basic_maths {
     }
 
     // Given two integers N1 and N2, find their greatest common divisor.
+    // Brute force approach - O(min(N1, N2))
     public static int findGcd(int N1, int N2) {
-        // Brute force approach - O(min(N1, N2))
         int gcd = 1;
         for (int i = 1; i <= Math.min(N1, N2); i++) {
             if (N1 % i == 0 && N2 % i == 0) {
@@ -104,12 +104,62 @@ public class basic_maths {
         return gcd;
     }
 
+    // Optimal approach - O(log(min(a, b)))
+    public static int findGcdEuclidean(int a, int b) {
+        while (a > 0 && b > 0) {
+            if (a > b)
+                a = a % b;
+            else
+                b = b % a;
+        }
+        if (a == 0) {
+            System.out.println("GCD is: " + b);
+            return b;
+        }
+        System.out.println("GCD is: " + a);
+        return a;
+    }
+
+    // Given an integer N, return true it is an Armstrong number otherwise return
+    // false.
+    // An Amrstrong number is a number that is equal to the sum of its own digits
+    // each raised to the power of the number of digits.
+    public static boolean isArmstrong(int num) {
+        int n = num;
+        int digits = (int) Math.log10(n) + 1;
+        int sum = 0;
+        while (n != 0) {
+            sum = sum + (int) Math.pow(n % 10, digits);
+            n = n / 10;
+        }
+        return sum == n;
+    }
+
+    // Given an integer N, check whether it is prime or not. A prime number is a
+    // number that is only divisible by 1 and itself and the total number of
+    // divisors is 2
+    public static boolean isPrime(int num) {
+        int divisors = 0;
+        for (int i = 1; i * i <= num; i++) {
+            if (num % i == 0) {
+                divisors++;
+                if (num / i != i) {
+                    divisors++;
+                }
+            }
+        }
+        return divisors == 2;
+    }
+
     public static void main(String[] args) {
         // extractDigits(5431);
         // countDigits(4565);
         // reverseIntDigits(46215);
         // isPalindrome(45544);
-        getDivisors(36);
-        findGcd(13, 11);
+        // getDivisors(36);
+        // findGcd(13, 11);
+        findGcdEuclidean(100, 400);
+        isArmstrong(123);
+        isPrime(13);
     }
 }
