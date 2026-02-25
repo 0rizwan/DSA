fce73f08d3c3f477a89623118f596fd4992f8cc3
0rizwan <fycsrizwan@gmail.com>
2026-02-25 17:31:38 +0530

    basic hashmap

--- dsa_with_java/src/basics/hashing.java
+++ dsa_with_java/src/basics/hashing.java
@@ -1,5 +1,7 @@
 package basics;
 
+import java.util.HashMap;
+
 public class hashing {
     static public void findFrequency(int q, int[] arr) {
         int[] hash = new int[arr.length + 1];
@@ -15,6 +17,36 @@ public class hashing {
             hash[str.charAt(i)] += 1;
         }
         System.out.println(hash[q]);
+    }
+
+    static public void frequencyInArrays(int[] arr, int b) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+        int maxFreq = 0, minFreq = n;
+        int maxElem = 0, minElem = 0;
+        for (HashMap.Entry<Integer, Integer> entry : map.entrySet()) {
+            int element = entry.getKey();
+            int count = entry.getValue();
+
+            if (count > maxFreq) {
+                maxFreq = count;
+                maxElem = element;
+            }
+            if (count < minFreq) {
+                minFreq = count;
+                minElem = element;
+            }
+            System.out.println(entry.getKey() + " >> " + entry.getValue());
+        }
+        System.out.println("The highest frequency element is: " + maxElem);
+        System.out.println("The lowest frequency element is: " + minElem);
     }
 
     public static void main(String[] args) {
