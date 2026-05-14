bd1e402fa55c5807159ff030937f22167d44fc60
0rizwan <fycsrizwan@gmail.com>
2026-05-14 19:25:22 +0530

    setMatrixZero optimal approach done

--- dsa_with_java/src/arrays/medium/setMatrixZero.java
+++ dsa_with_java/src/arrays/medium/setMatrixZero.java
@@ -42,6 +42,7 @@ public class setMatrixZero {
         }
     }
 
+    // Better approach using two extra arrays to track for rows and cols
     public static void setZeroesMatrix2(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
@@ -57,8 +58,6 @@ public class setMatrixZero {
                 }
             }
         }
-        System.out.println(Arrays.toString(rows));
-        System.out.println(Arrays.toString(cols));
         // Step 2: set elements to 0 based on marks
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
@@ -72,10 +71,74 @@ public class setMatrixZero {
         }
     }
 
+    // Optimal approach using first row and first col to track (no extra space) -
+    public static void setMatrixZero3(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+
+        // checking if 0 in first row
+        for (int i = 0; i < cols; i++) {
+            if (matrix[0][i] == 0) {
+                firstRowZero = true;
+                break;
+            }
+        }
+
+        // checking if 0 in first col
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                firstColZero = true;
+                break;
+            }
+        }
+
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if (firstRowZero) {
+            for (int i = 0; i < cols; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (firstColZero) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        System.out.println("");
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(Arrays.toString(matrix[i]));
+        }
+    }
+
     public static void main(String[] args) {
         int[][] matrix = { { 1, 1, 1, 1 }, { 1, 0, 1, 1 }, { 1, 1, 1, 1 } };
+        int[][] matrix2 = { { 0, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 } };
+        int[][] matrix3 = { { 0, 1, 1, 1 }, { 1, 0, 1, 0 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 } };
 
         setZeroesMatrix(matrix);
-        setZeroesMatrix2(matrix);
+        System.out.println("");
+        setZeroesMatrix2(matrix2);
+        System.out.println("");
+        setMatrixZero3(matrix3);
     }
 }
